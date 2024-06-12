be3b3b8bb0c7ae4fbea77163da9355402201f03c
abrilhf <abril.hernandez@davinci.edu.ar>
2024-06-12 20:52:14 -0300

    Se agregó archivo pdf archivos/

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,8 @@ function Contact() {
             <h1>Co<span className="letraN">n</span>tact</h1>
             <p>Let&apos;s Craft Something Extraordinary Together!</p>
             <a href="mailto:hernandezfloresabril1@gmail.com">hernandezfloresabril1@gmail.com</a>
+            <p>My CV</p>
+            <embed src="" type="" />b
         </div>
     )
 }
