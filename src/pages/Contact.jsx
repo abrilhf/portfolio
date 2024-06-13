b23fd148ba6d42df8b240cab2aaba3c25ee964ff
abrilhf <abril.hernandez@davinci.edu.ar>
2024-06-12 22:03:13 -0300

    Se agregó archivo pdf <a>

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,8 +4,9 @@ function Contact() {
             <h1>Co<span className="letraN">n</span>tact</h1>
             <p>Let&apos;s Craft Something Extraordinary Together!</p>
             <a href="mailto:hernandezfloresabril1@gmail.com">hernandezfloresabril1@gmail.com</a>
-            <p>My CV</p>
-            <embed src="" type="" />b
+            <div className="cv">
+                <a href="https://github.com/abrilhf/portfolio/blob/main/src/archivos/Curr%C3%ADculum%20Vitae%20Abril%20Hernandez%20Flores.pdf" target="_blank" rel="noreferrer">My CV</a>
+            </div>
         </div>
     )
 }
