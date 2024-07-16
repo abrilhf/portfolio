63ff36bdbc46d6c4c56f9fc8d5694a6c2cc45d20
abrilhf <abril.hernandez@davinci.edu.ar>
2024-07-16 13:42:38 -0300

    Agregamos para las metricas

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import App from './App'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle';
 import { BrowserRouter } from 'react-router-dom';
-import {NextUIProvider} from '@nextui-org/react'
+import {NextUIProvider} from '@nextui-org/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root'))
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root'))
 
 .render(
     <BrowserRouter>
+        <SpeedInsights />
         <NextUIProvider>
             <App/>
         </NextUIProvider>
